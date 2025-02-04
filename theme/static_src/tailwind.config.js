ca23aa44389cd1fd4fc3d9da5ac2aeb612cec4f1
Michael Gurnett <mgurnett@gmail.com>
2025-02-04 15:49:43 -0700

    it is working

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -42,7 +42,37 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+              primary: {
+                50: '#f0f9ff',
+                100: '#e0f2ff',
+                200: '#cce5ff',
+                300: '#b2d7ff',
+                400: '#87cefa',  // Your blue - slightly adjusted for the scale
+                500: '#38bdf8', // Your blue
+                600: '#0ea5e9',
+                700: '#0369a1',
+                800: '#075985',
+                900: '#0c4a6e',
+                950: '#111827',
+              },
+              secondary: {  // Gray scale
+                50: '#f9fafb',
+                100: '#f3f4f6',
+                200: '#e5e7eb',
+                300: '#d1d5db',
+                400: '#9ca3af',
+                500: '#6b7280',
+                600: '#4b5563',
+                700: '#374151',
+                800: '#1f2937',
+                900: '#111827',
+              },
+              background: '#f0f0f0', // Light gray background
+              whiteish: '#f8f8f8',
+            },
+        },
     },
     plugins: [
         /**

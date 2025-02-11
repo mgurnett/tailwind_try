4d2098c8d9deeca07b8c925b96db2049a2b4098d
Michael Gurnett <mgurnett@gmail.com>
2025-02-11 16:25:38 -0700

    added micropython

---
+++ micropython/minimal_GW/RTC.py
@@ -0,0 +1,48 @@
+import machine
+import time
+import utime
+import struct
+import socket
+import urequests
+
+# from archive.board_devices import *
+from wifi import *
+
+
+def time_string():
+    return f"{time.localtime()[0]}-{time.localtime()[1]}-{time.localtime()[2]} {time.localtime()[3]}:{time.localtime()[4]:02}:{time.localtime()[5]:02}"
+
+
+def set_time(hrs_offset=-5, debug=False):
+    NTP_DELTA = 2208988800
+    host = "pool.ntp.org"
+
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1B
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+    except:
+        return
+    finally:
+        s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    t = val - NTP_DELTA + hrs_offset * 3600
+    tm = time.gmtime(t)
+    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+
+
+if __name__ == "__main__":
+    ip = None
+    ip = get_wifi()
+    if ip:
+        print(f"ip: {ip}")
+    else:
+        ip = None
+        print(f"ip: NOT SET")
+
+    set_time(-5)
+    print(time.localtime())
---
+++ micropython/minimal_GW/wifi.py
@@ -0,0 +1,63 @@
+import utime
+from archive.board_devices import *
+import network  # handles connecting to WiFiimport machine
+
+# see new_wifi as it has some good ideas
+
+
+def wifi_connect(ssid, password, debug=False):
+    # Create an object, wlan, to create a connection from our code to the Pico W wireless chip.
+    # We use this connection to issue commands that will connect and check our Wi-Fi connection.
+    wlan = network.WLAN(network.STA_IF)
+    # Turn on the Raspberry Pi Pico W’s Wi-Fi.
+    wlan.active(True)
+    # Connect to your router using the SSID and PASSWORD stored in the secrets.py file.
+    wlan.connect(ssid, password)
+
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            #             print(f'wlan.status {str(wlan.status())}')
+            menu_item(0, f"wlan.status {str(wlan.status())}") if debug else None
+            break
+        max_wait -= 1
+        #         print('waiting for connection')
+        menu_item(1, f"waiting for connect {max_wait}") if debug else None
+        utime.sleep_ms(1000)
+
+    # Handle connection error
+    if wlan.status() != 3:
+        menu_item(3, "network connection failed") if debug else None
+        #         raise RuntimeError('network connection failed')
+        return None
+    else:
+        #         print('connected')
+        menu_item(2, "connected") if debug else None
+        status = wlan.ifconfig()
+        #         print( 'ip = ' + status[0] )
+        menu_item(3, "ip = " + status[0]) if debug else None
+        #         return status[0]
+        return status[0]
+
+
+def get_wifi(debug=False):
+    ip = None
+    connect_attempt = 5
+    while connect_attempt > 0 and ip == None:
+        ip = wifi_connect()
+        if ip == None:
+            connect_attempt = -1
+
+    if ip == None:
+        return None
+    else:
+        menu_item(4, f"IP: {ip}") if debug else None
+        menu_item(5, f"ca: {connect_attempt}") if debug else None
+        return ip
+
+
+if __name__ == "__main__":
+    clear_display()
+    new_ip = get_wifi()
+    if new_ip:
+        menu_item(5, f"Final: {new_ip}")

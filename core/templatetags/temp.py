83f7461b71aed5f7d136a8ad541f98cbb04b6ded
Michael Gurnett <mgurnett@gmail.com>
2025-04-30 13:00:09 -0600

    Small changes

--- core/templatetags/temp.py
+++ core/templatetags/temp.py
@@ -26,7 +26,7 @@ def temp_with_colour__html(value, alarm, alarm_state): #float, int, object
             else:
                 colour = ""
 
-        return mark_safe(f'<button class="w3-button w3-{colour}">{temp_value__str}</button>')
+        return mark_safe(f'<button class="button {colour}">{temp_value__str}</button>')
 
 @register.simple_tag
 def temp_text_colour__html(value, alarm, alarm_state): #float, int, object
@@ -34,20 +34,22 @@ def temp_text_colour__html(value, alarm, alarm_state): #float, int, object
         temp = round(value, 1)
 
         if alarm_state == 2:
-            colour = "purple"
+            colour = "#6686b9"
         else:
             if value > alarm:
-                colour = "red"
+                colour = "#f44336"
             elif (value + 5) > alarm:
                 colour = "orange"
             elif (value + 10) > alarm:
-                colour = "yellow"
+                # colour = "yellow"
+                colour = "#c4a448"
             elif (value + 15) > alarm:
-                colour = "khaki"
+                # colour = "khaki"
+                colour = "#46b480"
             else:
-                colour = ""
+                colour = "#999999"
 
-        return mark_safe(f'<span class="w3-text-{colour}">{temp}°C</span>')
+        return mark_safe(f'<span class="text-[{colour}]">{temp}°C</span>')
         
 @register.simple_tag
 def date_format(latest_update):
@@ -57,26 +59,26 @@ def date_format(latest_update):
         now = datetime.now()  # Use timezone.now()
         time_difference = now - latest_update
         if time_difference > timedelta(hours=1):
-            return mark_safe(f'<div class="w3-text-red">{latest_update.strftime("%a, %b %d @ %-I:%M:%S %p")}</div>')
+            return mark_safe(f'<div class="text-red">{latest_update.strftime("%a, %b %d @ %-I:%M:%S %p")}</div>')
         else:
-            return mark_safe(f'<div class="w3-text-green">{latest_update.strftime("%a, %b %d @ %-I:%M:%S %p")}</div>')
+            return mark_safe(f'<div class="text-green">{latest_update.strftime("%a, %b %d @ %-I:%M:%S %p")}</div>')
     
 @register.simple_tag
 def alarm_state__html(state):
     if state == 0:
-        return mark_safe('<button class="w3-button w3-green">No alarm</button>')
+        return mark_safe('<button class="button text-[#f44336]">ALARM</button>')
     elif state == 1:
-        return mark_safe('<button class="w3-button w3-red">ALARM</button>')
+        return mark_safe('<button class="button text-[#46b480]">OK</button>')
     else: 
-        return mark_safe('<button class="w3-button w3-purple">Alarm cleared</button>')
+        return mark_safe('<button class="button purple">Alarm cleared</button>')
     
 @register.simple_tag
 def battery_voltage__html(voltage):
     if voltage == None:
         return ""
     if voltage > 3.6:
-        return mark_safe(f'<span class="w3-text-green">{round(voltage, 1)}V</span>')
+        return mark_safe(f'<span class="text-[#46b480]">{round(voltage, 1)}V</span>')
     elif voltage < 3.6 and voltage > 3.1:
-        return mark_safe(f'<span class="w3-text-orange">{round(voltage, 1)}V</span>')
+        return mark_safe(f'<span class="text-[#c4a448]">{round(voltage, 1)}V</span>')
     else: 
-        return mark_safe(f'<span class="w3-text-red">{round(voltage, 1)}V</span>')
+        return mark_safe(f'<span class="text-[#f44336]">{round(voltage, 1)}V</span>')
